cfdf4c3d33402c91b6af894a13cef9a32650e107
MJ <mjsonofharry@gmail.com>
2022-05-15 16:50:07 -0400

    add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='name',
+    version='0.1',
+    description='Python client for Name.com API.',
+    author='MJ',
+    author_email='mjsonofharry@gmail.com',
+    packages=['name'],
+    install_requires=['requests']
+)
